a280edd49566c5dbe6c313756459d2489053a82d
Eric Ahnell <eric.ahnell@puttysoftware.com>
2019-12-27 12:09:40 -0500

    Create an engine config file, and populate it.

---
+++ laser-tank-improved/conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.identity = "com.puttysoftware.lasertankimproved"
+    t.version = "11.3"
+    t.window.title = "LaserTank"
+    -- Disable modules the game does not use
+    t.modules.video = false
+end
