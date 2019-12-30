6f9db45bfcdf8f92aa85a85878dff60fee080e41
Eric Ahnell <eric.ahnell@puttysoftware.com>
2019-12-30 10:43:34 -0500

    Begin the process of tidying the localization data.
    The menu structure data is in the process of being split into its own file. The accelerator data needs to be split off too. Keys for the non-menu localization entries need to be fixed.

---
+++ laser-tank-improved/assets/data/menus.lua
@@ -0,0 +1,36 @@
+return {
+	menus = {
+		items = 2,
+		item_0 = {
+			title = "game",
+			items = 1,
+			item_0 = {
+				title = "menu_file",
+				items = 19,
+				item_0 = "menu_file_new",
+				item_1 = "menu_file_load",
+				item_2 = "menu_file_skip",
+				item_3 = "menu_file_previous",
+				item_4 = "menu_file_last_played",
+				item_5 = "__SEPARATOR__",
+				item_6 = "menu_file_open_data",
+				item_7 = "__SEPARATOR__",
+				item_8 = "menu_file_high_scores",
+				item_9 = "menu_file_global_high_scores",
+				item_10 = "__SEPARATOR__",
+				item_11 = "menu_file_enable_recording",
+				item_12 = "menu_file_save_recording",
+				item_13 = "menu_file_resume_recording",
+				item_14 = "menu_file_load_recording",
+				item_15 = "__SEPARATOR__",
+				item_16 = "menu_file_print",
+				item_17 = "__SEPARATOR__",
+				item_18 = "menu_file_exit"
+			}
+		},
+		item_1 = {
+			title = "editor",
+			items = 0
+		}
+	}
+}
