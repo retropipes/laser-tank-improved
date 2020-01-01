9b1b6aefc8092a4320f1f3fc944339f895a3b35f
Eric Ahnell <eric.ahnell@puttysoftware.com>
2020-01-01 14:17:42 -0500

    Add the rest of the menu definitions to the menus data file.

--- laser-tank-improved/assets/data/menus.lua
+++ laser-tank-improved/assets/data/menus.lua
@@ -1,12 +1,12 @@
 return {
 	menus = {
-		items = 2,
+		num_items = 2,
 		item_0 = {
 			title = "game",
-			items = 2,
+			num_items = 5,
 			item_0 = {
 				title = "menu_file",
-				items = 19,
+				num_items = 19,
 				item_0 = "menu_file_new",
 				item_1 = "menu_file_load",
 				item_2 = "menu_file_skip",
@@ -28,18 +28,94 @@ return {
 				item_18 = "menu_file_exit"
 			},
 			item_1 = {
-				title = "move",
-				items = 5,
+				title = "menu_move",
+				num_items = 5,
 				item_0 = "menu_game_restart",
 				item_1 = "menu_game_undo",
 				item_2 = "menu_game_save_position",
 				item_3 = "menu_game_restore_position",
 				item_4 = "menu_game_replay"
+			},
+			item_2 = {
+				title = "menu_options",
+				num_items = 10
+				item_0 = "menu_options_animation",
+				item_1 = "menu_options_sound",
+				item_2 = "menu_options_remember_last_level",
+				item_3 = "menu_options_skip_completed_levels",
+				item_4 = "menu_options_auto_record",
+				item_5 = "menu_options_disable_warnings",
+				item_6 = "__SEPARATOR__",
+				item_7 = "menu_options_size",
+				item_7_has_subitems = true
+				item_7_subitems = {
+					num_items = 3
+					item_0 = "menu_options_size_small",
+					item_1 = "menu_options_size_medium",
+					item_2 = "menu_options_size_large"
+				},
+				item_8 = "menu_options_difficulty",
+				item_9 = "menu_options_graphics"
+			},
+			item_3 = {
+				title = "menu_editor",
+				num_items = 0
+			},
+			item_4 = {
+				title = "menu_help",
+				num_items = 6
+				item_0 = "menu_help_hint",
+				item_1 = "__SEPARATOR__",
+				item_2 = "menu_help_quick",
+				item_3 = "menu_help_index",
+				item_4 = "menu_help_changes",
+				item_5 = "menu_help_about"
 			}
 		},
 		item_1 = {
 			title = "editor",
-			items = 0
+			num_items = 6
+			item_0 = {
+				title = "editor_menu_editor",
+				num_items = 5
+				item_0 = "editor_menu_editor_clear",
+				item_1 = "editor_menu_editor_load",
+				item_2 = "editor_menu_editor_save",
+				item_3 = "editor_menu_editor_save_as",
+				item_4 = "editor_menu_editor_exit"
+			},
+			item_1 = {
+				title = "editor_menu_hint",
+				num_items = 0
+			},
+			item_2 = {
+				title = "editor_menu_shift",
+				num_items = 4
+				item_0 = "editor_menu_shift_right",
+				item_1 = "editor_menu_shift_left",
+				item_2 = "editor_menu_shift_up",
+				item_3 = "editor_menu_shift_down"
+			},
+			item_3 = {
+				title = "editor_menu_close",
+				num_items = 0
+			},
+			item_4 = {
+				title = "editor_menu_difficulty",
+				num_items = 5
+				item_0 = "editor_menu_difficulty_kids",
+				item_1 = "editor_menu_difficulty_easy",
+				item_2 = "editor_menu_difficulty_medium",
+				item_3 = "editor_menu_difficulty_hard",
+				item_4 = "editor_menu_difficulty_deadly"
+			},
+			item_5 = {
+				title = "editor_menu_help",
+				num_items = 3
+				item_0 = "editor_menu_help_editor",
+				item_1 = "editor_menu_help_creating_levels",
+				item_2 = "editor_menu_help_about"
+			}
 		}
 	}
 }
